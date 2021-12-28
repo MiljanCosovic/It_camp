cfd595641a10b7df92da18681563e9f3c870bd90
Miljan Cosovic <miljancosovicnp@gmail.com>
2021-12-28 17:58:11 +0100

    JavaScript exaple

---
+++ domaciJS2.js
@@ -0,0 +1,6 @@
+var a=prompt("Unesite prvi broj: ");
+var b=prompt("Unesite drugi broj: ");
+var c=prompt("Unesite treci broj broj: ");
+var max= Math.max(a,b,c);
+alert("Najveci broj je: " +max);
+
---
+++ domaciJS3.js
@@ -0,0 +1,7 @@
+var a=Number(prompt("Unesite prvi broj: "));
+var b=Number(prompt("Unesite drugi broj: "));
+var c=Number(prompt("Unesite treci broj broj: "));
+var niz=[]
+niz.push(a,b,c);
+niz.sort();
+alert("Sortirani niz je:" +niz);
