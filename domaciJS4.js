5c9270984256a9f874ebc09d28d8299510dc2c65
Miljan Cosovic <miljancosovicnp@gmail.com>
2021-12-28 18:08:58 +0100

    JavaScript exaple

---
+++ domaciJS4.js
@@ -0,0 +1,15 @@
+var a=prompt("Unesite prvu stranicu: ");
+var b=prompt("Unesite drugu straniuc: ");
+var c=prompt("Unesite trecu stranicu: ");
+
+if(a+b>c){
+    alert("Moguce je napraviti trougao");
+}
+else if(a==0 || b==0 ||c==0 )
+{
+    alert("Nije moguce napraviti trougao");
+}
+else {
+    alert("Nije moguce napraviti trougao");
+}
+
