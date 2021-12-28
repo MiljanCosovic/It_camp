af2a3def5208877399e22542ace92a0525122ae5
Miljan Cosovic <miljancosovicnp@gmail.com>
2021-12-28 12:11:21 +0100

    JavaScript exaple

---
+++ domaciJS1.js
@@ -0,0 +1,12 @@
+broj=prompt("Unesite neki broj: ");
+if(broj<0)
+{
+    alert("Broj je negativan")
+}
+else if(broj==0)
+{
+    alert("Uneti broj je nula");
+}
+else{
+    alert("Broj je pozitivan");
+}
