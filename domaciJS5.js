129e361ca18111e1933bc9b43b779ae605aa7666
Miljan Cosovic <miljancosovicnp@gmail.com>
2021-12-29 18:44:33 +0100

    JavaScript switch-case

---
+++ domaciJS5.js
@@ -0,0 +1,47 @@
+ a=Number(prompt("Unesite broj meseca"));
+
+ switch(a)
+ {
+     case 1:
+         alert("Januar");
+         break;
+         case 2:
+         alert("Februar");
+         break;
+         case 3:
+         alert("Mart");
+         break;
+         case 4:
+         alert("April");
+         break;
+         case 5:
+         alert("Maj");
+         break;
+         case 6:
+         alert("Jun");
+         break;
+         case 7:
+         alert("Jul");
+         break;
+         case 8:
+         alert("Avgust");
+         break;
+         case 9:
+         alert("Septembar");
+         break;
+         case 10:
+         alert("Oktobar");
+         break;
+         case 11:
+         alert("Novembar");
+         break;
+         case 12:
+         alert("Decembar");
+         break;
+
+         default:
+             alert("Uneli ste pogresan broj");
+
+ }
+
+
