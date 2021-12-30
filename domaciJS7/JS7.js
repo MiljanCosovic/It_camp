f2edac1b9c7c15ccbdf8380e9527daa224f34c00
Miljan Cosovic <miljancosovicnp@gmail.com>
2021-12-30 07:34:40 +0100

    JavaScript examples

---
+++ domaciJS7/JS7.js
@@ -0,0 +1,9 @@
+var broj = parseInt(prompt("Unesite jedan broj: "));
+   if(broj%2 == 0)
+   {
+       alert("Uneti broj je paran");
+   }
+   else
+   {
+       alert("Uneti broj je neparan");
+   }
