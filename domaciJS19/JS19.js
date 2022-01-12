642f52d2ee10c0c1a829eba834f282ff9e215c4c
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-01-12 21:49:04 +0100

    Examples of array in JavaScript

---
+++ domaciJS19/JS19.js
@@ -0,0 +1,10 @@
+var niz=[2,5,6,43,22,9,24,545,222,142]
+suma=0;
+for(var i=0;i<niz.length;i++)
+{
+    if(niz[i]%2==0)
+    {
+        suma+=niz[i];
+    }
+}
+console.log("Suma parnih brojeva je: " +suma);
