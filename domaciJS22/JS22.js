a9bf5a935faa007c4307258bf39c764a1b80a253
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-01-15 10:25:23 +0100

    Examples of array in JavaScript

---
+++ domaciJS22/JS22.js
@@ -0,0 +1,14 @@
+var b=[];
+var niz=[[1,2,1,24],[8,11,9,4],[7,0,7,27],[7,4,28,14],[3,10,26,7]];
+for(var i=0;i<niz.length;i++)
+{
+    console.log(niz[i])
+    for(var j=0;j<niz[i].length;j++)
+    {
+        console.log(niz[i][j]);
+        var el=niz[i][j];   
+        
+    }
+
+    
+}
