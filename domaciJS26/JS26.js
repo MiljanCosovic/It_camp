d9837a4f46862eef41270feaa0fab726727d5dc3
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-01-22 18:52:48 +0100

    Examples of function in JavaScript

--- domaciJS26/JS26.js
+++ domaciJS26/JS26.js
@@ -1,14 +1,40 @@
-var niz=[2, "asdf",4,"bdf",3234];
-var rez=[];
-for(i=0;i<niz.length;i++)
-{
-    if(typeof niz[i]==="number")
+// var niz=[2, "asdf",4,"bdf",3234];
+// var rez=[];
+// for(i=0;i<niz.length;i++)
+// {
+//     if(typeof niz[i]==="number")
+//     {
+//         rez.unshift(niz[i]);
+//     }
+//     else
+//     {
+//         rez.push(niz[i]);
+//     }
+// }
+// console.log(rez);
+
+
+
+
+function tekst(){
+    let txt = 'the quick fox';
+    var niz=txt.split(" ");
+    for(i=0;i<niz.length;i++)
     {
-        rez.unshift(niz[i]);
+        niz[i] = niz[i].charAt(0).toUpperCase() + niz[i].slice(1);
     }
-    else
-    {
-        rez.push(niz[i]);
-    }
+
+    var txt2 = niz.join(" ");
+
+
+
+    console.log(txt2);
+
 }
-console.log(rez);
+
+tekst();
+
+
+
+
+
