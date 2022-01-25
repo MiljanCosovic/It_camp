afce5b6014c21ed36651d3d600b3bfd2f95e7535
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-01-25 17:23:16 +0100

    Examples of function in JavaScript

--- domaciJS26/JS26.js
+++ domaciJS26/JS26.js
@@ -16,25 +16,50 @@
 
 
 
-function tekst(){
-    let txt = 'the quick fox';
-    var niz=txt.split(" ");
-    for(i=0;i<niz.length;i++)
-    {
-        niz[i] = niz[i].charAt(0).toUpperCase() + niz[i].slice(1);
-    }
+// function tekst(){
+//     let txt = 'the quick fox';
+//     var niz=txt.split(" ");
+//     for(i=0;i<niz.length;i++)
+//     {
+//         niz[i] = niz[i].charAt(0).toUpperCase() + niz[i].slice(1);
+//     }
 
-    var txt2 = niz.join(" ");
+//     var txt2 = niz.join(" ");
 
 
 
-    console.log(txt2);
+//     console.log(txt2);
 
+// }
+
+
+
+// tekst();
+
+
+
+
+
+function deli(iznos, novcanica){
+   
+    brojNovcanica = Math.floor(iznos / novcanica);
+
+return brojNovcanica;
 }
+function broji(suma, novcanice)
+{
+for (let i = 0; i < novcanice.length; i++) {
+    if(suma!=0){
+        pom = parseInt(novcanice[i])
+        var brNovcanica = deli(suma, pom);
+        console.log(brNovcanica + " novcanica od " + pom + " dinara");
+        var noviIznos = suma - (pom * brNovcanica)
+    } 
+    suma=noviIznos;
+}
+}
+broji(2342,[200, 50, 25, 20, 5,2,1]);
 
-
-
-tekst();
 
 
 
