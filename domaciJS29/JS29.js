46af06fd46b9170c9d7e98b919117171239b9840
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-01-27 18:05:23 +0100

    Examples of function in JavaScript

---
+++ domaciJS29/JS29.js
@@ -0,0 +1,12 @@
+function digitize(n) {
+    
+    n=n.toString()
+    var b=[]
+    for(var i=0;i<n.length;i++)
+    {
+        
+        b.unshift(n[i]);
+    }
+    console.log(b);
+  }
+  digitize(1234);
