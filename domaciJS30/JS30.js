1581a8b45f97bb9ee4decfecb13b50578025c984
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-02-01 17:04:44 +0100

    Examples of function in JavaScript

---
+++ domaciJS30/JS30.js
@@ -0,0 +1,14 @@
+function niz()
+{
+    data=[3,24,54,123,32,64];
+    var b=0;
+
+    for(i=0;i<data.length;i++)
+    {
+        b+=data[i];
+    }
+    c=b/data.length;
+    console.log(c);
+}
+
+niz();
