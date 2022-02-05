85422505c70c770c1b393b639b8fa9abafd08f42
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-02-05 17:47:15 +0100

    Examples of function in JavaScript

---
+++ domaciJ35/JS35.js
@@ -0,0 +1,21 @@
+function nbYear(p0, percent, aug, p) {
+    var brojac=0;
+    if(aug>0)
+    {
+    while(p0<=p)
+    {
+    p0=p0+(p0*(percent/100)+aug);
+    brojac++
+    }
+    }
+    else
+    {
+        return;
+    }
+    console.log(brojac)
+
+}
+nbYear(1000,2,50,1200);
+nbYear(1500, 5, 100, 5000);
+nbYear(1500000, 2.5, 10000, 2000000)
+nbYear(1500000, 0.25, 1000, 2000000)
