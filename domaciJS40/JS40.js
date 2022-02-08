bc87c32da00d3db13d5af0d06a0a5370b9bd4f79
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-02-08 08:28:13 +0100

    Examples of function in JavaScript

---
+++ domaciJS40/JS40.js
@@ -0,0 +1,15 @@
+function tek(s) {
+  pomarr = s.split(" ");
+  rez = [];
+  for (let i = 0; i < pomarr.length; i++) {
+    if (pomarr[i] !== pomarr[i + 1]) {
+      rez.push(pomarr[i]);
+    }
+  }
+  return rez.join(" ");
+}
+console.log(
+  tek(
+    "alfa beta beta gama gama gama delta alpha beta beta gama gama gama delta"
+  )
+);
