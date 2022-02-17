c27e349f3c806093f2b9a11dd0ca1009ac085742
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-02-17 15:34:09 +0100

    Examples with objects in JavaScript

---
+++ domaciJS50/JS50.js
@@ -0,0 +1,19 @@
+
+function ima(prop){
+  covek={
+    name:"Miljan",
+    surname:"Ljanmi",
+    location:"Novi Pazar",
+    nickname:"Ljanmi",
+  }
+ if(prop in covek)
+ {
+    console.log("Objekat ima property");
+ }
+ else
+ {
+     console.log("Objekat nema propery");
+ }
+}
+ima("location");
+ima("lastname");
