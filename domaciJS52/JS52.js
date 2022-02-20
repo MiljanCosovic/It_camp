a4a945b48acea03859e8e359df392e834fdae02c
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-02-20 12:55:48 +0100

    Examples with objects in JavaScript

--- domaciJS52/JS52.js
+++ domaciJS52/JS52.js
@@ -7,22 +7,23 @@ function data(biblioteka) {
   console.log(b);
 }
 
-data([ 
-    {
-        author: 'Bill Gates',
-        title: 'The Road Ahead',
-        readingStatus: true,
-        age: 16
-    },
-    {
-        author: 'Steve Jobs',
-        title: 'Walter Isaacson',
-        readingStatus: true,
-        age:21
-    },
-    {
-        author: 'Suzanne Collins',
-        title:  'Mockingjay: The Final Book of The Hunger Games', 
-        readingStatus: false,
-        age: 18
-    }]);
+data([
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true,
+    age: 16,
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true,
+    age: 21,
+  },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+    age: 18,
+  },
+]);
